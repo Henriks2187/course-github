38f54a4394d5a1f27b6316c12038156c7933f852
Henrique Paiva <paiva.henrique@hotmail.com>
2019-06-09 18:35:30 -0300

    Primeira Implementacao Teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 	public static void main(String[] args) {
-		
-		System.out.println("Teste de primeiro commit");
-		
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Bem vindo ao cadastro de produtos.");
+		System.out.println("Digite o nome do produto: ");
+		String productName = sc.nextLine();
+		System.out.println("Digite o preço do produto: ");
+		double price = sc.nextDouble();
+		System.out.println("Digite a quantidade so estoque: ");
+		int quantity = sc.nextInt();
+
+		Product product = new Product(productName, price, quantity);
+
+		System.out.println("Produdo Cadastrado: ");
+		System.out.println(product);
+
+		sc.close();
 	}
 }
---
+++ src/entities/Product.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Product {
+
+	private String productName;
+	private double productPrice;
+	private int productQuantity;
+
+	public Product(String productName, double productPrice, int productQuantity) {
+		this.productName = productName;
+		this.productPrice = productPrice;
+		this.productQuantity = productQuantity;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(double productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public int getProductQuantity() {
+		return productQuantity;
+	}
+
+	public void setProductQuantity(int productQuantity) {
+		this.productQuantity = productQuantity;
+	}
+
+	public String toString() {
+		return "Product [Product=" + productName + ", Price=" + productPrice + ", Quantity=" + productQuantity + "]";
+	}
+
+}
